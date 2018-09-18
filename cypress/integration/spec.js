6a688fac3f4c94229c9558d94f687bd2650e2414
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-09-17 21:51:45 -0400

    solved at 91%

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,17 +2,47 @@
 describe('test', () => {
   it('solves', () => {
     cy.visit('https://slikts.github.io/js-equality-game')
+
+    // get all values to compare
+    let values = []
     cy.get('thead th')
       .each((el$, column) => {
         if (!el$.text()) {
           return
         }
-        console.log('el$ column %d is %s', column, el$.text())
+        // only '{}' gives back undefined, but should be an empty object {}
+        values.push(eval(el$.text()))
+      })
+      .then(() => {
+        console.log('%d value(s) to compare', values.length)
+        console.log(values)
+        assert(values.length > 0, 'expected some values to compare')
 
-        cy.get('tbody tr').each((row$, row) => {
-          const x = row$.find('th').text()
-          console.log('row X', x)
+        let clicks = 0
+        cy.get('tbody tr td').each((cell$, k) => {
+          const row = Math.floor(k / values.length)
+          const col = k % values.length
+          if (col <= row) {
+            // only interested in filling the upper right triangle of the field
+            return
+          }
+
+          const x = values[row]
+          const y = values[col]
+          console.log(x, '==', y, '?', x == y)
+
+          if (x == y) {
+            clicks += 1
+            cy.wrap(cell$).click()
+            // and make sure the field has registered our click
+            cy.contains('.Results-flags', clicks)
+          }
         })
       })
+
+      cy.log('time to find out the truth ...')
+      cy.contains('Show Results').click()
+      // because we cannot `eval({})` correctly, we miss 9% of the grade 🙃
+      cy.contains('.Results-face', '91% correct')
   })
 })
