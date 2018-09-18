0e3ec4bdd69947de96d3b5d7600ee1ca2a1ebb48
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-09-17 23:54:53 -0400

    more testing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-equality-game
+
+Solves (or almost solves) [https://slikts.github.io/js-equality-game/](https://slikts.github.io/js-equality-game/)
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,48 +1,48 @@
 /// <reference types="cypress" />
-describe('test', () => {
-  it('solves', () => {
-    cy.visit('https://slikts.github.io/js-equality-game')
-
-    // get all values to compare
-    let values = []
-    cy.get('thead th')
-      .each((el$, column) => {
-        if (!el$.text()) {
+it('js-equality', () => {
+  cy.visit('https://slikts.github.io/js-equality-game')
+  // get all values to compare
+  let values = []
+  cy.get('thead th')
+    .each((el$) => {
+      if (!el$.text()) {
+        return
+      }
+      // only '{}' gives back undefined, but should be an empty object {}
+      values.push(eval(el$.text()))
+    })
+    .then(() => {
+      console.log('%d value(s) to compare', values.length)
+      console.log(values)
+      assert(values.length > 0, 'expected some values to compare')
+    })
+    .then(() => {
+      let clicks = 0
+      cy.get('tbody td').each((cell$, k) => {
+        const row = Math.floor(k / values.length)
+        const col = k % values.length
+        if (col <= row) {
+          // only interested in filling the upper right triangle of the field
           return
         }
-        // only '{}' gives back undefined, but should be an empty object {}
-        values.push(eval(el$.text()))
+
+        const x = values[row]
+        const y = values[col]
+        console.log(x, '==', y, '?', x == y)
+
+        if (x == y) {
+          // click on the cell
+          cy.wrap(cell$).click()
+          // and make sure the field has registered our click
+          clicks += 1
+          cy.contains('.Results-flags .Score', clicks)
+        }
       })
-      .then(() => {
-        console.log('%d value(s) to compare', values.length)
-        console.log(values)
-        assert(values.length > 0, 'expected some values to compare')
-
-        let clicks = 0
-        cy.get('tbody tr td').each((cell$, k) => {
-          const row = Math.floor(k / values.length)
-          const col = k % values.length
-          if (col <= row) {
-            // only interested in filling the upper right triangle of the field
-            return
-          }
-
-          const x = values[row]
-          const y = values[col]
-          console.log(x, '==', y, '?', x == y)
-
-          if (x == y) {
-            clicks += 1
-            cy.wrap(cell$).click()
-            // and make sure the field has registered our click
-            cy.contains('.Results-flags', clicks)
-          }
-        })
-      })
-
+    })
+    .then(() => {
       cy.log('time to find out the truth ...')
       cy.contains('Show Results').click()
       // because we cannot `eval({})` correctly, we miss 9% of the grade 🙃
       cy.contains('.Results-face', '91% correct')
-  })
+    })
 })
